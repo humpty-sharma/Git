1e9dfdee8b35f38de6a1c7fdb4dc8a1c4a2917c6
humpty-sharma <joshia00@gmail.com>
2019-08-30 02:59:56 -0700

    updated

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 class simple{
 	public static void main(String args[]) throws InterruptedException{
-		Thread.sleep(1000);
-		System.out.println("Hello Java");
+		Thread.sleep(2000);
+		System.out.println("Hello Java Updated");
 	}
 }
